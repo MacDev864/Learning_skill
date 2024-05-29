41fc8638d5e23ed7498382a97b09e54991c4600e
MacDev864 <113145526+MacDev864@users.noreply.github.com>
2024-05-29 14:37:53 +0700

    Update routes.js

--- dist/routes.js
+++ dist/routes.js
@@ -9,12 +9,18 @@ var _note = require("./controllers/notes/note.Controller");
 var _auth = require("./controllers/auth/auth.Controller");
 // 
 var route = (0, _express.Router)();
+/*----------------------------------------Note------------------------------------- */
 route.post("/create", _note.create);
 route.get("/get/notes", _note.getAll);
 route.get("/get/:id", _note.getNoteById);
 route.patch("/update/:id", _note.updateNoteById);
 route.patch("/remove/:id", _note.updateNoteById);
 route["delete"]("/delete/:id", _note.removeNoteById);
+/*----------------------------------------------------------------------------------*/
+
+/*----------------------------------------Auth------------------------------------- */
 route.post("/register", _auth.register);
 route.post("/login", _auth.login);
+route.post("/logout", _auth.logout);
+/*----------------------------------------------------------------------------------*/
 var _default = exports["default"] = route;
