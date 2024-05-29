5a07c834cd9a428b72c927cd854b6bed26b85c72
MacDev864 <113145526+MacDev864@users.noreply.github.com>
2024-05-29 14:38:16 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,36 @@
 import { Router } from "express";
 import { create,getAll,getNoteById,updateNoteById,removeNoteById } from "./controllers/notes/note.Controller";
-import { login, register} from "./controllers/auth/auth.Controller";
+import { login, logout, register} from "./controllers/auth/auth.Controller";
 // 
 const route = Router()
+/*----------------------------------------Note------------------------------------- */
 route.post("/create",create);
 route.get("/get/notes",getAll);
 route.get("/get/:id",getNoteById);
 route.patch("/update/:id",updateNoteById);
 route.patch("/remove/:id",updateNoteById);
 route.delete("/delete/:id",removeNoteById);
+/*----------------------------------------------------------------------------------*/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------------------Auth------------------------------------- */
 route.post("/register",register);
 route.post("/login",login);
+route.post("/logout",logout);
+/*----------------------------------------------------------------------------------*/
+
 
 
 
